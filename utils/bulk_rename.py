bb8a41554b24583278c9585d363fa30d010dbfb0
levequer <raphael.leveque@ttpsc.pl>
2023-11-24 18:12:55 +0100

    u

---
+++ utils/bulk_rename.py
@@ -0,0 +1,25 @@
+import os
+
+def rename_files(root_directory, old_string, new_string):
+    """
+    Recursively renames files in subdirectories by replacing old_string with new_string.
+    :param root_directory: The directory to start searching from.
+    :param old_string: The string in the filename to be replaced.
+    :param new_string: The string to replace with.
+    """
+    for dirpath, dirnames, filenames in os.walk(root_directory):
+        for filename in filenames:
+            if old_string in filename:
+                new_filename = filename.replace(old_string, new_string)
+                old_file_path = os.path.join(dirpath, filename)
+                new_file_path = os.path.join(dirpath, new_filename)
+
+                print(f"Renaming '{old_file_path}' to '{new_file_path}'")
+                os.rename(old_file_path, new_file_path)
+
+# Example Usage
+root_dir = 'D:/WTPyScripts/input' # Replace with your directory path
+old_str = 'OLD' # Replace with the text you want to replace
+new_str = 'NEW' # Replace with the new text
+
+rename_files(root_dir, old_str, new_str)
---
+++ utils/bulk_replace_content.py
@@ -0,0 +1,32 @@
+import os
+
+def replace_in_files(root_directory, old_string, new_string):
+    """
+    Recursively replaces old_string with new_string in the contents of files.
+    :param root_directory: The directory to start searching from.
+    :param old_string: The string in the file contents to be replaced.
+    :param new_string: The string to replace with.
+    """
+    for dirpath, dirnames, filenames in os.walk(root_directory):
+        for filename in filenames:
+            file_path = os.path.join(dirpath, filename)
+
+            # Read the contents of the file
+            with open(file_path, 'r', encoding='utf-8') as file:
+                contents = file.read()
+
+            # Replace the old string with the new string
+            if old_string in contents:
+                new_contents = contents.replace(old_string, new_string)
+
+                # Write the new contents back to the file
+                with open(file_path, 'w', encoding='utf-8') as file:
+                    file.write(new_contents)
+                print(f"Replaced text in '{file_path}'")
+
+# Example Usage
+root_dir = 'D:/WTPyScripts/input' # Replace with your directory path
+old_str = 'OLD'  # Replace with the text you want to replace
+new_str = 'NEW'  # Replace with the new text
+
+replace_in_files(root_dir, old_str, new_str)
