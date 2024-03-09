0206d6449208586b250986de1f919c706a2eeafc
raphlev <raphleveque@hotmail.fr>
2024-03-09 10:42:15 +0100

    u

--- windchill/enums/test_merge_xml_enumerated_values_with_new_entries.bat
+++ windchill/enums/test_merge_xml_enumerated_values_with_new_entries.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal
 :: Batch command to test tmerge_xml_enumerated_values_with_new_entries.py with 4 scenarios. It creates 4 Test* folders with resulted files.
 :: <ROOT> .\windchill\enums\test_merge_xml_enumerated_values_with_new_entries.bat
 
@@ -11,75 +12,70 @@ set "PYTHON_EXE=python.exe"
 :: In a batch (*.bat) script, the ampersand & character is interpreted as a command separator. 
 :: To include an ampersand (&) in a string that you're echoing to a file, you need to escape it by using the ^ character before the ampersand. 
 
-:: Scenario 1: Test sort by name all entries (existing and new)
-set "TEST_DIR=%BASE_DIR%\windchill\enums\test"
+:: Scenario: Test sort by name all entries (existing and new)
+:: Preserve the original selectable value of existing entries matching new entries
+:: Force selectable value at false for the new entries added to existing entries
+set "TEST_DIR=%BASE_DIR%\windchill\enums\testName_pes_f"
 set "INPUT_CSV=%TEST_DIR%\csvInput.csv"
+call :CreateTestDir
+call :CreateCSV
+REM sort by name all entries (existing and new)
+"%PYTHON_EXE%" "%PYTHON_SCRIPT%" -i "%INPUT_XML%" -n "%INPUT_CSV%" -o "%TEST_DIR%" -pes -f
+
+:: Scenario: Test sort by name all entries (existing and new)
+set "TEST_DIR=%BASE_DIR%\windchill\enums\testName"
+set "INPUT_CSV=%TEST_DIR%\csvInput.csv"
+call :CreateTestDir
+call :CreateCSV
+REM sort by name all entries (existing and new)
+"%PYTHON_EXE%" "%PYTHON_SCRIPT%" -i "%INPUT_XML%" -n "%INPUT_CSV%" -o "%TEST_DIR%"
+
+:: Scenario: Test preserve existing sorting at begininng - and sort by name new entries at the end
+set "TEST_DIR=%BASE_DIR%\windchill\enums\testName_po"
+set "INPUT_CSV=%TEST_DIR%\csvInput.csv"
+call :CreateTestDir
+call :CreateCSV
+REM preserve existing sorting at begininng - and sort by name new entries at the end
+"%PYTHON_EXE%" "%PYTHON_SCRIPT%" -i "%INPUT_XML%" -n "%INPUT_CSV%" -o "%TEST_DIR%" -po
+
+:: Scenario: Test sort by displayName all entries (existing and new)
+set "TEST_DIR=%BASE_DIR%\windchill\enums\testDisplayName"
+set "INPUT_CSV=%TEST_DIR%\csvInput.csv"
+call :CreateTestDir
+call :CreateCSV
+REM sort by displayName all entries (existing and new)
+"%PYTHON_EXE%" "%PYTHON_SCRIPT%" -i "%INPUT_XML%" -n "%INPUT_CSV%" -o "%TEST_DIR%" -s displayName
+
+:: Scenario: Test preserve existing sorting at begininng - and sort by displayName new entries at the end
+set "TEST_DIR=%BASE_DIR%\windchill\enums\testDisplayName_po"
+set "INPUT_CSV=%TEST_DIR%\csvInput.csv"
+call :CreateTestDir
+call :CreateCSV
+REM preserve existing sorting at begininng - and sort by displayName new entries at the end
+"%PYTHON_EXE%" "%PYTHON_SCRIPT%" -i "%INPUT_XML%" -n "%INPUT_CSV%" -o "%TEST_DIR%" -po -s displayName
+
+:: End of the main script
+goto :eof
+
+:: Function-like section
 :: delete and create test directory
+:CreateTestDir
 rd /s /q "%TEST_DIR%"
 mkdir "%TEST_DIR%"
 if not exist "%TEST_DIR%" (
     echo Failed to create or access the test directory.
     exit /b
 )
+goto :eof
+
+:: create and populate the csvInput.cs
+:CreateCSV
 :: csvInput input file creation in excel:
 :: =CONCATENATE("echo ",C2, "~", D2, "~>>""%INPUT_CSV%""")
 echo name~displayName~csvlocale_fr>"%INPUT_CSV%"
 echo H0405~A129 (M)~>>"%INPUT_CSV%"
 echo T9998~Cars ^& Formula1~>>"%INPUT_CSV%"
-:: Execute Python scripts
-REM sort by name all entries (existing and new)
-"%PYTHON_EXE%" "%PYTHON_SCRIPT%" -i "%INPUT_XML%" -n "%INPUT_CSV%" -o "%TEST_DIR%"
+goto :eof
 
-:: Scenario 2: Test preserve existing sorting at begininng - and sort by name new entries at the end
-set "TEST_DIR=%BASE_DIR%\windchill\enums\testName_p"
-set "INPUT_CSV=%TEST_DIR%\csvInput.csv"
-:: delete and create test directory
-rd /s /q "%TEST_DIR%"
-mkdir "%TEST_DIR%"
-if not exist "%TEST_DIR%" (
-    echo Failed to create or access the test directory.
-    exit /b
-)
-:: csvInput input file creation
-echo name~displayName~csvlocale_fr>"%INPUT_CSV%"
-echo H0405~A129 (M)~>>"%INPUT_CSV%"
-echo T9998~Cars ^& Formula1~>>"%INPUT_CSV%"
-:: Execute Python scripts
-REM preserve existing sorting at begininng - and sort by name new entries at the end
-"%PYTHON_EXE%" "%PYTHON_SCRIPT%" -i "%INPUT_XML%" -n "%INPUT_CSV%" -o "%TEST_DIR%" -po
-
-:: Scenario 3: Test sort by displayName all entries (existing and new)
-set "TEST_DIR=%BASE_DIR%\windchill\enums\testDisplayName"
-set "INPUT_CSV=%TEST_DIR%\csvInput.csv"
-:: delete and create test directory
-rd /s /q "%TEST_DIR%"
-mkdir "%TEST_DIR%"
-if not exist "%TEST_DIR%" (
-    echo Failed to create or access the test directory.
-    exit /b
-)
-:: csvInput input file creation
-echo name~displayName~csvlocale_fr>"%INPUT_CSV%"
-echo H0405~A129 (M)~>>"%INPUT_CSV%"
-echo T9998~Cars ^& Formula1~>>"%INPUT_CSV%"
-:: Execute Python scripts
-REM sort by displayName all entries (existing and new)
-"%PYTHON_EXE%" "%PYTHON_SCRIPT%" -i "%INPUT_XML%" -n "%INPUT_CSV%" -o "%TEST_DIR%" -s displayName
-
-:: Scenario 4: Test preserve existing sorting at begininng - and sort by displayName new entries at the end
-set "TEST_DIR=%BASE_DIR%\windchill\enums\testDisplayName_p"
-set "INPUT_CSV=%TEST_DIR%\csvInput.csv"
-:: delete and create test directory
-rd /s /q "%TEST_DIR%"
-mkdir "%TEST_DIR%"
-if not exist "%TEST_DIR%" (
-    echo Failed to create or access the test directory.
-    exit /b
-)
-:: csvInput input file creation
-echo name~displayName~csvlocale_fr>"%INPUT_CSV%"
-echo H0405~A129 (M)~>>"%INPUT_CSV%"
-echo T9998~Cars ^& Formula1~>>"%INPUT_CSV%"
-:: Execute Python scripts
-REM preserve existing sorting at begininng - and sort by displayName new entries at the end
-"%PYTHON_EXE%" "%PYTHON_SCRIPT%" -i "%INPUT_XML%" -n "%INPUT_CSV%" -o "%TEST_DIR%" -po -s displayName
+:eof  REM End of file
+endlocal
