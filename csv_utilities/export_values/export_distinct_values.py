b0e2df449d0ed31da2284db1f869773ade44173b
levequer <raphael.leveque@ttpsc.pl>
2024-05-28 14:34:36 +0200

    u

--- csv_utilities/export_values/export_distinct_values.py
+++ csv_utilities/export_values/export_distinct_values.py
@@ -2,7 +2,7 @@
 Export Unique Values from CSV Based on Column Position or Name with Custom Separator
 
 This script reads from a specified input CSV file, extracts unique values from a specified column
-(by position or name), and outputs total count of distinct values, and outputs these unique values along with their occurrence counts to a new CSV file.
+(by position or name), and outputs these unique values.
 Users can specify the column either by its position in the header row (starting by 1) or by its name. 
 Empty values in the specified column are replaced with "NULL" in the output. Additionally, the script 
 supports specifying a custom field delimiter for both reading the input file and writing to the output file, 
@@ -38,7 +38,7 @@ import argparse
 
 def export_distinct_values(input_csv, output_csv, position=None, column_name=None, separator=';'):
     index = None
-    value_counts = {}
+    unique_values = set()  # Use a set to store unique values
 
     try:
         with open(input_csv, mode='r', encoding='utf-8') as infile:
@@ -61,24 +61,16 @@ def export_distinct_values(input_csv, output_csv, position=None, column_name=Non
                 if len(row) > index:
                     value = row[index]
                     value = value if value.strip() else "NULL"
-                    if value in value_counts:
-                        value_counts[value] += 1
-                    else:
-                        value_counts[value] = 1
+                    unique_values.add(value)  # Add value to the set
 
-        sorted_values_counts = sorted(value_counts.items())
-        total_unique_values = len(value_counts)
-        total_occurrences = sum(value_counts.values())
+        sorted_values = sorted(unique_values)  # Sort the unique values
 
         with open(output_csv, mode='w', encoding='utf-8', newline='') as outfile:
             print(f"Writing to {output_csv}")
             writer = csv.writer(outfile, delimiter=separator)
-            writer.writerow(['Total Unique Values', total_unique_values])
-            writer.writerow(['Total Occurrences', total_occurrences])
-            writer.writerow([])  # Blank row
-            writer.writerow(['Value', 'Count'])
-            for value, count in sorted_values_counts:
-                writer.writerow([value, count])
+            writer.writerow(['Value'])  # Write header
+            for value in sorted_values:
+                writer.writerow([value])  # Write each unique value
 
         print("File has been written successfully.")
 
